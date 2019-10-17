a321139e17de2f1eaa96938021acfb5f4186d02d
NRRajiB <lakshminrr27@gmail.com>
2019-10-17 10:40:54 +0530

    updated json

--- org.zalando/src/test/java/TestRunnerZalando/TestRunner.java
+++ org.zalando/src/test/java/TestRunnerZalando/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources\\feature_file\\zalando.feature",glue="com.zalando.Stepdefinition", dryRun=false, 
-monochrome=true, tags= {"@Feature4","@SmokeTest"})
+monochrome=true, tags= {"@Feature4","@SmokeTest"}, plugin= {"json:target/cucumber.json"})
 public class TestRunner {
 
 }
